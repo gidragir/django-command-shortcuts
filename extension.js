253c907695cbf4e4767b78e3a50bd83a0232d6c7
Gidragir <ilyabalter1@gmail.com>
2022-04-02 02:44:34 +0600

    ver 1.0

--- extension.js
+++ extension.js
@@ -1,33 +1,40 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "django-command-shortcuts" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('django-command-shortcuts.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Django command shortcuts!');
+	let runserver = vscode.commands.registerCommand('django-command-shortcuts.runserver', function () {
+		if (vscode.window.activeTerminal) {
+			vscode.window.activeTerminal.sendText('python ./taskmanager/manage.py runserver')	
+		}
 	});
 
-	context.subscriptions.push(disposable);
+	let makemigrations = vscode.commands.registerCommand('django-command-shortcuts.makemigrations', function () {
+		if (vscode.window.activeTerminal) {
+			vscode.window.activeTerminal.sendText('python ./taskmanager/manage.py makemigrations')	
+		}
+	});
+
+	let migrate = vscode.commands.registerCommand('django-command-shortcuts.migrate', function () {
+		if (vscode.window.activeTerminal) {
+			vscode.window.activeTerminal.sendText('python ./taskmanager/manage.py migrate')	
+		}
+	});
+
+	let env = vscode.commands.registerCommand('django-command-shortcuts.env', function () {
+		if (vscode.window.activeTerminal) {
+			vscode.window.activeTerminal.sendText('./env/Scripts/Activate.ps1')	
+		}
+	});
+
+	context.subscriptions.push(runserver);
+	context.subscriptions.push(makemigrations);
+	context.subscriptions.push(migrate);
+	context.subscriptions.push(env);
 }
 
-// this method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
