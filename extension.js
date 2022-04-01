32f2a40d84b57ec129d20fde2e39e247cf0d9921
Gidragir <ilyabalter1@gmail.com>
2022-04-02 04:41:10 +0600

    ver 1.03

--- extension.js
+++ extension.js
@@ -1,31 +1,47 @@
 const vscode = require('vscode');
 
 /**
- * @param {vscode.ExtensionContext} context
- */
+* @param {vscode.ExtensionContext} context
+*/
+
 function activate(context) {
 
+	function sendTextToTerminal(result, uriFolder, command) {
+		let path = result[0].path;
+		let pureUri = path.replace(uriFolder, '.')
+		vscode.window.activeTerminal.sendText('python ' + pureUri + ' ' + command)
+	}
+
+	let uriFolder = vscode.workspace.workspaceFolders[0].uri.path;
+
 	let runserver = vscode.commands.registerCommand('django-command-shortcuts.runserver', function () {
+
 		if (vscode.window.activeTerminal) {
-			vscode.window.activeTerminal.sendText('python ./taskmanager/manage.py runserver')	
+			vscode.workspace.findFiles('*/manage.py', uriFolder).then(function (result) {
+				sendTextToTerminal(result, uriFolder, 'runserver');
+			})
 		}
 	});
 
 	let makemigrations = vscode.commands.registerCommand('django-command-shortcuts.makemigrations', function () {
 		if (vscode.window.activeTerminal) {
-			vscode.window.activeTerminal.sendText('python ./taskmanager/manage.py makemigrations')	
+			vscode.workspace.findFiles('*/manage.py', uriFolder).then(function (result) {
+				sendTextToTerminal(result, uriFolder, 'makemigrations');
+			})
 		}
 	});
 
 	let migrate = vscode.commands.registerCommand('django-command-shortcuts.migrate', function () {
 		if (vscode.window.activeTerminal) {
-			vscode.window.activeTerminal.sendText('python ./taskmanager/manage.py migrate')	
+			vscode.workspace.findFiles('*/manage.py', uriFolder).then(function (result) {
+				sendTextToTerminal(result, uriFolder, 'migrate');
+			})
 		}
 	});
 
 	let env = vscode.commands.registerCommand('django-command-shortcuts.env', function () {
 		if (vscode.window.activeTerminal) {
-			vscode.window.activeTerminal.sendText('./env/Scripts/Activate.ps1')	
+			vscode.window.activeTerminal.sendText('./env/Scripts/Activate.ps1')
 		}
 	});
 
